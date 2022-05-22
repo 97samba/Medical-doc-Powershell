898460b37294c3ec58016ac08fc9cea06f73c0ed
samba <97samba@gmail.com>
2022-05-22 20:00:22 +0200

    final step

--- src/Install_dependencies.ps1
+++ src/Install_dependencies.ps1
@@ -1,4 +1,4 @@
-$REQUIREMENTS = @(
+﻿$REQUIREMENTS = @(
     @{
         name    = "ImportExcel" 
         version = "7.4.1" 
@@ -13,25 +13,34 @@ function verify-requirements() {
     $execution_policy = $(Get-ExecutionPolicy -Scope CurrentUser)
 
     if ($execution_policy -ne "Unrestricted") {
-        write-host "Changement des droits d'exécution en cours" -ForegroundColor Green
+        # write-host "Changement des droits d'exécution en cours" -ForegroundColor Green
 
         Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
 
     }
 
     $REQUIREMENTS | ForEach-Object {
-        if (Get-InstalledModule -Name $_.name) {
+        if (Get-InstalledModule -Name $_.name -ErrorAction SilentlyContinue) {
             Write-Host ===> $_.name déja installé -ForegroundColor Green 
         }
         else {
-            write-host "Téléchargement du Module $_.name assurez-vous d'avoir une connexion internet stable" 
-            Install-Module -Name ===> $_.name -RequiredVersion $_.version -Scope CurrentUser
+            Write-Host ===> $_.name manquant -ForegroundColor Red 
+
+            write-host "Téléchargement du Module $($_.name) assurez-vous d'avoir une connexion internet stable" 
+            Install-Module -Name $($_.name) -RequiredVersion $($_.version) -Scope CurrentUser -Confirm:$False -Force
         }
     }  
-    Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser
+    try {
+
+        Set-ExecutionPolicy -ExecutionPolicy $execution_policy -Scope CurrentUser | Out-Null
+    }
+    catch {
+        write-host "Fin d'installation"
+    }
   
 }
 
-Start-sleep -s 5
 verify-requirements
+write-host Vous pouvez lancer la création de scripts 
+
 Start-sleep -s 5
